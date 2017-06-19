b418850e898ec2d7cd76d5f1febf8920f2994958
Björn Skoglund <bjorn.skoglund@icloud.com>
2017-06-19 17:48:35 -0400

    Added support for debug flag.

--- mockidp/main.py
+++ mockidp/main.py
@@ -1,10 +1,17 @@
 # coding: utf-8
+from optparse import OptionParser
 
-from mockidp.routes import conf
 from mockidp import app
+from mockidp.routes import conf
 
 
 def main(argv):
+    parser = OptionParser()
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug", default=False,
+                      help="Launch app in auto-reloading debug mode")
+
+    options, args = parser.parse_args(args=argv)
 
     for sp in conf['service_providers']:
         print(f"Known Service Provider {sp['name']}")
@@ -12,4 +19,4 @@ def main(argv):
     for username, data in conf['users'].items():
         print(f"Loaded user {username}")
 
-    app.run(debug=True, port=conf.get('port', 5000))
+    app.run(debug=options.debug, port=conf.get('port', 5000))

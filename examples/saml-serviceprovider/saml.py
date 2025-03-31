fde9e0cad05b5a5f1a95bd8fee00b832ecd50c0b
Björn Skoglund <bjorn.skoglund@icloud.com>
2025-03-31 16:55:48 -0400

    Remove settings.json from input

--- examples/saml-serviceprovider/saml.py
+++ examples/saml-serviceprovider/saml.py
@@ -5,11 +5,15 @@ from jinja2 import Environment, FileSystemLoader
 
 TEMPLATES_PATH = os.path.join(os.path.dirname(__file__), 'templates')
 
+
+
 def init_ssl():
     loader = FileSystemLoader(searchpath=TEMPLATES_PATH)
     env = Environment(loader=loader)
     env.filters['b64encode'] = lambda s: base64.b64encode(s.encode('utf-8')).decode('utf-8')
     template = env.get_template('settings.json')
     # TODO: Generate SSL certs
-    print(template.render(servide_provider_cert='foobar'))
+
     # Render to file
+    with open(os.path.join(os.path.dirname(__file__), 'saml/settings.json'), 'w') as f:
+        f.write(template.render(servide_provider_cert='foobar'))
